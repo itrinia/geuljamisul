93cb3eef1519886ff53c85e3ab9f660a11d2e9c7
ileene santoso <ileenesantoso@gmail.com>
2024-05-04 12:24:32 +0700

    done smua

---
+++ geuljamisul/infoView.swift
@@ -0,0 +1,18 @@
+//
+//  infoView.swift
+//  geuljamisul
+//
+//  Created by Ileene Trinia Santoso on 04/05/24.
+//
+
+import SwiftUI
+
+struct infoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    infoView()
+}
